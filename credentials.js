bd66492fe312c5d21fa929d0e30906eeac588b4d
Kevin Kwok <antimatter15@gmail.com>
2017-09-22 16:05:00 -0700

    fixing trailing commas

--- credentials.js
+++ credentials.js
@@ -3,5 +3,5 @@ module.exports = {
 	user: process.env.PGUSER || process.env.USER,
 	database: process.env.PGDATABASE || process.env.USER,
 	password: process.env.PGPASSWORD,
-	port: process.env.PGPORT || 5432,
+	port: process.env.PGPORT || 5432
 }
